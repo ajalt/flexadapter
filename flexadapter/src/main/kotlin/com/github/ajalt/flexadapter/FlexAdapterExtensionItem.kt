aadae9062d2b4f03bff824ba0809f35c6d63a9a8
AJ Alt <aj@alt>
2017-02-04 13:32:06 -0800

    Rework item taxonomy.

--- flexadapter/src/main/kotlin/com/github/ajalt/flexadapter/FlexAdapterExtensionItem.kt
+++ flexadapter/src/main/kotlin/com/github/ajalt/flexadapter/FlexAdapterExtensionItem.kt
@@ -12,7 +12,8 @@ import android.view.ViewGroup
  * Definition and creation of the view holder is taken care of, so subclasses just need define a
  * binder function.
  *
- * @constructor You can pass values to the constructor instead of overriding [dragDirs], [swipeDirs], and [span]
+ * @constructor You can pass values to the constructor instead of overriding [dragDirs],
+ * [swipeDirs], and [span] manually.
  *
  * @param layoutRes The layout resource for items of this type.
  * @param dragDirs Equivalent to overriding [FlexAdapterItem.dragDirs]
@@ -24,15 +25,52 @@ abstract class FlexAdapterExtensionItem(@LayoutRes val layoutRes: Int,
                                         override val swipeDirs: Int = 0,
                                         override val span: Int = 1) :
         FlexAdapterItem<FlexAdapterExtensionItem.ViewHolder>() {
+    /**
+     * Bind this item's data to its inflated view.
+     *
+     * @param itemView The view inflated from the [viewHolderFactory].
+     * @param position The index of this item in [FlexAdapter2.items]
+     */
     abstract fun bindItemView(itemView: View, position: Int)
 
     override fun viewHolderFactory(): (ViewGroup) -> ViewHolder = {
         ViewHolder(LayoutInflater.from(it.context).inflate(layoutRes, it, false))
     }
 
-    override fun bindViewHolder(holder: ViewHolder, position: Int) {
+    override final fun bindViewHolder(holder: ViewHolder, position: Int) {
         bindItemView(holder.itemView, position)
     }
 
+    /** A ViewHolder that relies on kotlin android extensions for caching. */
     class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
+}
+
+
+/**
+ * A selectable adapter item that's designed for use with the Kotlin Extensions for Android.
+ *
+ * @see FlexAdapterExtensionItem
+ * @see FlexAdapterSelectableItem
+ */
+abstract class FlexAdapterSelectableExtensionItem(@LayoutRes val layoutRes: Int,
+                                                  override val dragDirs: Int = 0,
+                                                  override val swipeDirs: Int = 0,
+                                                  override val span: Int = 1) :
+        FlexAdapterSelectableItem<FlexAdapterExtensionItem.ViewHolder>() {
+    /**
+     * Bind this item's data to its inflated view.
+     *
+     * @param itemView The view inflated from the [viewHolderFactory].
+     * @param selected Whether or not this item is marked as selected.
+     * @param position The index of this item in [FlexAdapter2.items]
+     */
+    abstract fun bindItemView(itemView: View, selected: Boolean, position: Int)
+
+    override fun viewHolderFactory(): (ViewGroup) -> FlexAdapterExtensionItem.ViewHolder = {
+        FlexAdapterExtensionItem.ViewHolder(LayoutInflater.from(it.context).inflate(layoutRes, it, false))
+    }
+
+    override final fun bindViewHolder(holder: FlexAdapterExtensionItem.ViewHolder, selected: Boolean, position: Int) {
+        bindItemView(holder.itemView, selected, position)
+    }
 }
